5e8b384adfe3959d603f27e7935f8d7590350070
RJ <128684285+Aar-J@users.noreply.github.com>
2024-08-24 16:52:21 +0530

    Add files via upload

---
+++ wro_feProg/RaspberryCodes/StopMotor.py
@@ -0,0 +1,9 @@
+import pigpio
+from time import sleep
+
+pi = pigpio.pi()
+pi.set_mode(18, pigpio.OUTPUT)
+pi.set_mode(13,pigpio.OUTPUT)
+pi.set_servo_pulsewidth(18,1200)
+pi.set_servo_pulsewidth(13,1500)
+sleep(0.1)
